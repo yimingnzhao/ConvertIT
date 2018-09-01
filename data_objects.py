16502ac240f50e588d9db68b1a1a05fca603f94a
root <root@DESKTOP-9DQ67BP.localdomain>
2018-08-31 23:58:14 -0700

    finished Unit class

--- data_objects.py
+++ data_objects.py
@@ -2,7 +2,7 @@ from google.appengine.ext import ndb
 
 class Unit(ndb.Model):
     name = ndb.StringProperty(required=True)
+    type = ndb.StringProperty(required=True)
     abbreviation = ndb.StringProperty(required=True)
-    convert_units = ndb.StringProperty(repreated=True)
-    convert_values = ndb.IntegerProperty(repeated=True)
-    
+    convert_units = ndb.StringProperty(repeated=True)
+    convert_values = ndb.FloatProperty(repeated=True)
